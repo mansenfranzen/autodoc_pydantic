b35d5282f1a7052b29506bfacae8126d4fefba5a
mansenfranzen <franz.woellert@gmail.com>
2021-04-07 20:40:56 +0200

    Add `test_model_with_config` and `test_model_plain_show_json`.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -85,3 +85,72 @@ def test_model_with_field_validator(app, autodocument):
         '         - :py:obj:`field <target.model.ModelWithFieldValidator.field>`',
         ''
     ]
+
+
+@pytest.mark.sphinx('html', testroot='ext-autodoc_pydantic')
+def test_model_with_config(app, autodocument):
+    options_app = dict(autodoc_pydantic_model_show_schema=False)
+    options_doc = dict(members=None)
+    actual = autodocument(app=app,
+                          documenter='pydantic_model',
+                          object_path='target.model.ModelWithConfig',
+                          options_doc=options_doc,
+                          options_app=options_app)
+
+    assert list(actual) == [
+        '',
+        '.. py:pydantic_model:: ModelWithConfig',
+        '   :module: target.model',
+        '',
+        '   Model with Config.',
+        '',
+        '   :Config:',
+        '      - **allow_mutation**: *bool = True*',
+        '',
+        '   .. py:pydantic_config_class:: ModelWithConfig.Config()',
+        '      :module: target.model',
+        '',
+        '      With Doc String.',
+        '',
+        '',
+        '      .. py:attribute:: ModelWithConfig.Config.allow_mutation',
+        '         :module: target.model',
+        '         :value: True',
+        '',
+        '         FooBar.',
+        '']
+
+
+@pytest.mark.sphinx('html', testroot='ext-autodoc_pydantic')
+def test_model_plain_show_json(app, autodocument):
+    actual = autodocument(app=app,
+                          documenter='pydantic_model',
+                          object_path='target.model.PlainModel')
+
+    assert list(actual) == [
+        '',
+        '.. py:pydantic_model:: PlainModel',
+        '   :module: target.model',
+        '',
+        '   Model Plain.',
+        '',
+        '',
+        '   .. raw:: html',
+        '',
+        '      <p><details>',
+        '      <summary><a>Show JSON schema</a></summary>',
+        '',
+        '   .. code-block:: json',
+        '',
+        '      {',
+        '         "title": "PlainModel",',
+        '         "description": "Model Plain.",',
+        '         "type": "object",',
+        '         "properties": {}',
+        '      }',
+        '',
+        '   .. raw:: html',
+        '',
+        '      </details></p>',
+        '',
+        '']
