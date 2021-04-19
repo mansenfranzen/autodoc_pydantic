3b345acef6d69962e52c78dd5eb938b0367f6cae
Franz Wöllert <franz.woellert@gmail.com>
2021-04-19 23:40:17 +0200

    Reorder classes. Update `PydanticConfigClassDocumenter`.

--- sphinxcontrib/autodoc_pydantic/autodoc.py
+++ sphinxcontrib/autodoc_pydantic/autodoc.py
@@ -47,170 +47,6 @@ tpl_collapse = """
 """
 
 
-class PydanticFieldDocumenter(AttributeDocumenter):
-    objtype = 'pydantic_field'
-    directivetype = 'pydantic_field'
-    priority = 10 + AttributeDocumenter.priority
-    option_spec = dict(AttributeDocumenter.option_spec)
-    option_spec.update(
-        {"field-show-default": option_default_true,
-         "field_show_constraints": option_default_true,
-         "field_list_validators": option_default_true,
-         "field_doc_policy": option_one_of_factory({"both", "docstring", "description"})})
-    member_order = 0
-
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.pyautodoc = PydanticAutoDoc(self)
-
-    @classmethod
-    def can_document_member(cls,
-                            member: Any,
-                            membername: str,
-                            isattr: bool,
-                            parent: Any) -> bool:
-        """Filter only pydantic fields.
-
-        """
-
-        is_val = super().can_document_member(member, membername, isattr, parent)
-        is_parent_model = is_pydantic_model(parent.object)
-        return is_val and is_parent_model and isattr
-
-    def add_directive_header(self, sig: str) -> None:
-        """Add default value to header.
-
-        """
-
-        super().add_directive_header(sig)
-        if self.pyautodoc.get_option_value("field-show-default"):
-            self.add_default_value()
-
-
-    def add_default_value(self):
-        """Adds default value.
-
-        """
-
-        name = self.objpath[-1]
-        wrapper = ModelWrapper(self.parent)
-        default = wrapper.get_field_properties_by_name(name).get("default")
-        if default is not None:
-            value = object_description(default)
-            sourcename = self.get_sourcename()
-            self.add_line('   :value: ' + value, sourcename)
-
-    def add_content(self,
-                    more_content: Optional[StringList],
-                    no_docstring: bool = False
-                    ) -> None:
-
-        doc_policy = self.pyautodoc.get_option_value("field_doc_policy")
-        if doc_policy in ("docstring", "both"):
-            super().add_content(more_content, no_docstring)
-        if doc_policy in ("both", "description"):
-            self.add_description()
-
-        if self.pyautodoc.get_option_value("field_show_constraints"):
-            self.add_constraints()
-
-        if self.pyautodoc.get_option_value("field_list_validators"):
-            self.add_validators()
-
-    def add_constraints(self):
-        """Adds section showing all defined constraints.
-
-        """
-
-        name = self.objpath[-1]
-        wrapper = ModelWrapper(self.parent)
-        field = wrapper.get_field_object_by_name(name)
-
-        constraints = get_field_schema_validations(field)
-        constraints = {key: value for key, value in constraints.items()
-                       if key not in {"env_names"}}
-
-        if constraints:
-            source_name = self.get_sourcename()
-            self.add_line(":Constraints:", source_name)
-            for key, value in constraints.items():
-                line = f"   - **{key}** = {value}"
-                self.add_line(line, source_name)
-
-    def add_description(self):
-        """Adds description from schema if present.
-
-        """
-
-        name = self.objpath[-1]
-        wrapper = ModelWrapper(self.parent)
-        properties = wrapper.get_field_properties_by_name(name)
-
-        description = properties.get("description")
-        if description is not None:
-            source_name = self.get_sourcename()
-            self.add_line(description, source_name)
-            self.add_line("", source_name)
-
-    def add_validators(self):
-        """Add section with all validators that process this field.
-
-        """
-
-        name = self.objpath[-1]
-        wrapper = ModelWrapper(self.parent)
-
-        validators = wrapper.get_validators_for_field(name)
-        if validators:
-            source_name = self.get_sourcename()
-            self.add_line(":Validated by:", source_name)
-            for validator in validators:
-                line = f"   - :py:obj:`{validator.name} <{validator.ref}>`"
-                self.add_line(line, source_name)
-
-
-class PydanticConfigClassDocumenter(ClassDocumenter):
-    """Represents specialized Documenter subclass for pydantic model
-    configuration.
-
-    """
-
-    objtype = 'pydantic_config'
-    directivetype = 'pydantic_config'
-    option_spec = ClassDocumenter.option_spec.copy()
-    option_spec.update({"undoc-members": option_default_true,
-                        "config-signature_prefix": unchanged})
-    member_order = 100
-    priority = 10 + ClassDocumenter.priority
-
-    def __init__(self, *args: Any) -> None:
-        super().__init__(*args)
-        self.pyautodoc = PydanticAutoDoc(self)
-        self.pyautodoc.set_default_option("undoc-members")
-        self.pyautodoc.set_default_option_with_value("members", ALL)
-        self.pyautodoc.pass_option_to_directive("config-signature_prefix")
-
-        if self.options.get("undoc-members") is False:
-            self.options.pop("undoc-members")
-
-    @classmethod
-    def can_document_member(cls,
-                            member: Any,
-                            membername: str,
-                            isattr: bool,
-                            parent: Any) -> bool:
-        """Filter only pydantic model configurations.
-
-        """
-
-        is_val = super().can_document_member(member, membername, isattr, parent)
-        is_parent_model = is_pydantic_model(parent.object)
-        is_config = membername == "Config"
-        is_class = isinstance(member, type)
-        return is_val and is_parent_model and is_config and is_class
-
-
-
 class PydanticModelDocumenter(ClassDocumenter):
     """Represents specialized Documenter subclass for pydantic models.
 
@@ -245,7 +81,6 @@ class PydanticModelDocumenter(ClassDocumenter):
         self.pyautodoc = PydanticAutoDoc(self)
 
         self.pyautodoc.set_default_option_with_value("members", ALL)
-
         if self.options.get("undoc-members") is False:
             self.options.pop("undoc-members")
 
@@ -399,6 +234,129 @@ class PydanticSettingsDocumenter(PydanticModelDocumenter):
             return False
 
 
+
+class PydanticFieldDocumenter(AttributeDocumenter):
+    objtype = 'pydantic_field'
+    directivetype = 'pydantic_field'
+    priority = 10 + AttributeDocumenter.priority
+    option_spec = dict(AttributeDocumenter.option_spec)
+    option_spec.update(
+        {"field-show-default": option_default_true,
+         "field_show_constraints": option_default_true,
+         "field_list_validators": option_default_true,
+         "field_doc_policy": option_one_of_factory({"both", "docstring", "description"})})
+    member_order = 0
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.pyautodoc = PydanticAutoDoc(self)
+
+    @classmethod
+    def can_document_member(cls,
+                            member: Any,
+                            membername: str,
+                            isattr: bool,
+                            parent: Any) -> bool:
+        """Filter only pydantic fields.
+
+        """
+
+        is_val = super().can_document_member(member, membername, isattr, parent)
+        is_parent_model = is_pydantic_model(parent.object)
+        return is_val and is_parent_model and isattr
+
+    def add_directive_header(self, sig: str) -> None:
+        """Add default value to header.
+
+        """
+
+        super().add_directive_header(sig)
+        if self.pyautodoc.get_option_value("field-show-default"):
+            self.add_default_value()
+
+
+    def add_default_value(self):
+        """Adds default value.
+
+        """
+
+        name = self.objpath[-1]
+        wrapper = ModelWrapper(self.parent)
+        default = wrapper.get_field_properties_by_name(name).get("default")
+        if default is not None:
+            value = object_description(default)
+            sourcename = self.get_sourcename()
+            self.add_line('   :value: ' + value, sourcename)
+
+    def add_content(self,
+                    more_content: Optional[StringList],
+                    no_docstring: bool = False
+                    ) -> None:
+
+        doc_policy = self.pyautodoc.get_option_value("field_doc_policy")
+        if doc_policy in ("docstring", "both"):
+            super().add_content(more_content, no_docstring)
+        if doc_policy in ("both", "description"):
+            self.add_description()
+
+        if self.pyautodoc.get_option_value("field_show_constraints"):
+            self.add_constraints()
+
+        if self.pyautodoc.get_option_value("field_list_validators"):
+            self.add_validators()
+
+    def add_constraints(self):
+        """Adds section showing all defined constraints.
+
+        """
+
+        name = self.objpath[-1]
+        wrapper = ModelWrapper(self.parent)
+        field = wrapper.get_field_object_by_name(name)
+
+        constraints = get_field_schema_validations(field)
+        constraints = {key: value for key, value in constraints.items()
+                       if key not in {"env_names"}}
+
+        if constraints:
+            source_name = self.get_sourcename()
+            self.add_line(":Constraints:", source_name)
+            for key, value in constraints.items():
+                line = f"   - **{key}** = {value}"
+                self.add_line(line, source_name)
+
+    def add_description(self):
+        """Adds description from schema if present.
+
+        """
+
+        name = self.objpath[-1]
+        wrapper = ModelWrapper(self.parent)
+        properties = wrapper.get_field_properties_by_name(name)
+
+        description = properties.get("description")
+        if description is not None:
+            source_name = self.get_sourcename()
+            self.add_line(description, source_name)
+            self.add_line("", source_name)
+
+    def add_validators(self):
+        """Add section with all validators that process this field.
+
+        """
+
+        name = self.objpath[-1]
+        wrapper = ModelWrapper(self.parent)
+
+        validators = wrapper.get_validators_for_field(name)
+        if validators:
+            source_name = self.get_sourcename()
+            self.add_line(":Validated by:", source_name)
+            for validator in validators:
+                line = f"   - :py:obj:`{validator.name} <{validator.ref}>`"
+                self.add_line(line, source_name)
+
+
 class PydanticValidatorDocumenter(MethodDocumenter):
     """Represents specialized Documenter subclass for pydantic validators.
 
@@ -471,3 +429,48 @@ class PydanticValidatorDocumenter(MethodDocumenter):
             self.add_line(line, source_name)
 
         self.add_line("", source_name)
+
+
+class PydanticConfigClassDocumenter(ClassDocumenter):
+    """Represents specialized Documenter subclass for pydantic model
+    configuration.
+
+    """
+
+    objtype = 'pydantic_config'
+    directivetype = 'pydantic_config'
+    option_spec = ClassDocumenter.option_spec.copy()
+    option_spec.update({"members": option_members,
+                        "config-signature-prefix": unchanged,
+                        "__doc_disable_except__": option_list_like})
+    member_order = 100
+    priority = 10 + ClassDocumenter.priority
+
+    pyautodoc_pass_to_directive = (
+        "config-signature-prefix",
+    )
+
+
+    def __init__(self, *args: Any) -> None:
+        super().__init__(*args)
+        self.pyautodoc = PydanticAutoDoc(self)
+
+        self.pyautodoc.set_default_option_with_value("members", ALL)
+        if self.options.get("members"):
+            self.options["undoc-members"] = True
+
+    @classmethod
+    def can_document_member(cls,
+                            member: Any,
+                            membername: str,
+                            isattr: bool,
+                            parent: Any) -> bool:
+        """Filter only pydantic model configurations.
+
+        """
+
+        is_val = super().can_document_member(member, membername, isattr, parent)
+        is_parent_model = is_pydantic_model(parent.object)
+        is_config = membername == "Config"
+        is_class = isinstance(member, type)
+        return is_val and is_parent_model and is_config and is_class
