cef7d63bf2e8f09cea965878b26569069afcd733
Franz Wöllert <franz.woellert@heidelberg.com>
2021-04-23 15:11:34 +0200

    Update the main example showing features of autodoc pydantic.

--- tests/roots/test-ext-autodoc_pydantic/target/__init__.py
+++ tests/roots/test-ext-autodoc_pydantic/target/__init__.py
@@ -7,13 +7,20 @@ class ExampleSettings(BaseSettings):
 
     """
 
-    field_with_constraints: int = Field(5, ge=0, le=100)
-    """Shows constraints within doc string."""
+    field_plain_with_validator: int = 100
+    """Show standard field with type annotation."""
 
-    field_with_validator: str = "FooBar"
+    field_with_validator_and_alias: str = Field("FooBar", alias="BarFoo")
     """Shows corresponding validator with link/anchor."""
 
-    @validator("field_with_validator")
+    field_with_constraints_and_description: int = Field(
+        default=5,
+        ge=0,
+        le=100,
+        description="Shows constraints within doc string."
+    )
+
+    @validator("field_with_validator_and_alias", "field_plain_with_validator")
     def check_max_length_ten(cls, v):
         """Show corresponding field with link/anchor.
 
@@ -24,3 +31,4 @@ class ExampleSettings(BaseSettings):
 
     class Config:
         env_prefix = "foo_"
+        allow_mutation = True
