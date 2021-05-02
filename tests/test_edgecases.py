74f448f2d83e74515eb0b5b27d8cbc08aef0bd4d
mansenfranzen <franz.woellert@gmail.com>
2021-05-02 16:14:04 +0200

    Add tests for `currentmodule` and `FakeDirective.`

--- tests/test_edgecases.py
+++ tests/test_edgecases.py
@@ -1,6 +1,16 @@
 """This module contains tests for edgecases.
 
 """
+import pytest
+from sphinx.ext.autosummary import FakeDirective, get_documenter
+
+from sphinxcontrib.autodoc_pydantic import (
+    PydanticModelDocumenter,
+    PydanticSettingsDocumenter,
+    PydanticFieldDocumenter,
+    PydanticValidatorDocumenter,
+    PydanticConfigClassDocumenter
+)
 
 
 def test_not_json_compliant(autodocument):
@@ -46,3 +56,90 @@ def test_not_json_compliant(autodocument):
         '      :value: <target.edgecases.NoJsonSerializer object>',
         ''
     ]
+
+
+def test_current_module_model(parse_rst):
+    """Ensure that using current module does not break any features.
+
+    This relates to issue #12.
+
+    """
+
+    input_rst = ['.. py:currentmodule:: target.example_model',
+                 '',
+                 '.. autopydantic_model:: ExampleModel',
+                 '   :model-show-json: True',
+                 '   :model-show-config-member: False',
+                 '   :model-show-config-summary: True',
+                 '   :model-show-validator-members: False',
+                 '   :model-show-validator-summary: False',
+                 '   :model-hide-paramlist: True',
+                 '   :undoc-members: True',
+                 '   :members: True',
+                 '   :member-order: alphabetical',
+                 '   :model-signature-prefix: pydantic_model',
+                 '   :field-list-validators: True',
+                 '   :field-doc-policy: both',
+                 '   :field-show-constraints: True',
+                 '   :field-show-alias: True',
+                 '   :field-show-default: True',
+                 '   :field-signature-prefix: field',
+                 '   :validator-signature-prefix: validator',
+                 '   :validator-replace-signature: True',
+                 '   :validator-list-fields: True',
+                 '   :config-signature-prefix: config',
+                 '']
+
+    parse_rst(input_rst,
+              conf={"extensions": ["sphinxcontrib.autodoc_pydantic"]})
+
+
+def test_current_module_settings(parse_rst):
+    """Ensure that using current module does not break any features.
+
+    This relates to issue #12.
+
+    """
+
+    input_rst = ['.. py:currentmodule:: target.example_setting',
+                 '',
+                 '.. autopydantic_settings:: ExampleSettings',
+                 '   :settings-show-json: True',
+                 '   :settings-show-config-member: False',
+                 '   :settings-show-config-summary: True',
+                 '   :settings-show-validator-members: False',
+                 '   :settings-show-validator-summary: False',
+                 '   :settings-hide-paramlist: True',
+                 '   :undoc-members: True',
+                 '   :members: True',
+                 '   :member-order: alphabetical',
+                 '   :settings-signature-prefix: pydantic_settings',
+                 '   :field-list-validators: True',
+                 '   :field-doc-policy: both',
+                 '   :field-show-constraints: True',
+                 '   :field-show-alias: True',
+                 '   :field-show-default: True',
+                 '   :field-signature-prefix: field',
+                 '   :validator-signature-prefix: validator',
+                 '   :validator-replace-signature: True',
+                 '   :validator-list-fields: True',
+                 '   :config-signature-prefix: config',
+                 '']
+
+    parse_rst(input_rst,
+              conf={"extensions": ["sphinxcontrib.autodoc_pydantic"]})
+
+
+@pytest.mark.parametrize("klass", [PydanticModelDocumenter,
+                                   PydanticSettingsDocumenter,
+                                   PydanticFieldDocumenter,
+                                   PydanticValidatorDocumenter,
+                                   PydanticConfigClassDocumenter])
+def test_autosummary(klass):
+    """Ensure that using autosummary's `FakeDirective` works with
+    pydantic autodocumenters.
+
+    This relates to issue #11.
+    """
+
+    klass(FakeDirective(), "")
