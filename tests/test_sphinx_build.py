c7067828e6a7b751eeea811ffcc737389e7bd1ea
Franz Wöllert <franz.woellert@gmail.com>
2021-05-08 14:28:54 +0200

    Fix path incompatibility on unix.

--- tests/test_sphinx_build.py
+++ tests/test_sphinx_build.py
@@ -1,11 +1,14 @@
 from sphinx.cmd.build import build_main
-
+from pathlib import Path
 
 def test_sphinx_build_without_warnings(tmpdir, recwarn):
     """Ensure that the build succeeds and no warning is raised.
 
     """
 
-    status_code = build_main(["../docs/source", str(tmpdir)])
+    path_root = Path(__file__).parents[1]
+    path_docs = path_root.joinpath("docs", "source")
+
+    status_code = build_main([str(path_docs), str(tmpdir)])
     assert status_code == 0
     assert len(recwarn) == 0
