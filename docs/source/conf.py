fe11714654a5e8d14d2b974142ec77906d767594
Franz Wöllert <franz.woellert@heidelberg.com>
2021-09-13 17:57:22 +0200

    Remove module names from autodoc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,3 +75,5 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+add_module_names = False
