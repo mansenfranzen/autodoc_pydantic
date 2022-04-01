c29155c58f306d2ed8ca874d288da9c54c8a722f
Franz Wöllert <franz.woellert@heidelberg.com>
2022-04-01 22:20:32 +0200

    Add `_is_json_serializable` to handle composite types.

--- sphinxcontrib/autodoc_pydantic/inspection.py
+++ sphinxcontrib/autodoc_pydantic/inspection.py
@@ -170,13 +170,25 @@ class FieldInspector(BaseInspectionComposite):
 
     def is_json_serializable(self, field_name: str) -> bool:
         """Check if given pydantic field is JSON serializable by calling
-        pydantic's `model.json()` method. Custom objects might not be
+        pydantic's `model.schema()` method. Custom objects might not be
         serializable and hence would break JSON schema generation.
 
         """
 
         field = self.get(field_name)
+        return self._is_json_serializable(field)
 
+    @classmethod
+    def _is_json_serializable(cls, field: ModelField):
+        """Ensure JSON serializabiltiy for given pydantic `ModelField`.
+
+        """
+
+        # check for sub fields in case of `Union` or alike, see #98
+        if field.sub_fields:
+            return all([cls._is_json_serializable(sub_field)
+                        for sub_field in field.sub_fields])
+        
         class Cfg:
             arbitrary_types_allowed = True
 
@@ -196,7 +208,7 @@ class FieldInspector(BaseInspectionComposite):
 
         return [name for name in self.names
                 if not self.is_json_serializable(name)]
-
+    
     def __bool__(self):
         """Equals to False if no fields are present.
 
