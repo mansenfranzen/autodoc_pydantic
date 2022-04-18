3606d93b43a1b27bbde3e76759fc7c08cf510027
Franz Wöllert <franz.woellert@heidelberg.com>
2022-04-18 19:58:53 +0200

    Update version - ci skip.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2022, Franz Wöllert'
 author = 'Franz Wöllert'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.2'
+release = '1.7.0'
 
 # -- General configuration ---------------------------------------------------
 
