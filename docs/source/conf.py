d6ef69f5b1c51aa3b21ebc9388a9403cc3f1c7c5
Franz Wöllert <franz.woellert@heidelberg.com>
2023-07-22 16:29:34 +0200

    Bump version - ci skip.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2022, Franz Wöllert'
 author = 'Franz Wöllert'
 
 # The full version, including alpha/beta/rc tags
-release = '1.9.0'
+release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
