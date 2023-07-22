476f84236d31006580a05e2da754913f9a105582
Franz Wöllert <franz.woellert@heidelberg.com>
2023-07-22 12:25:11 +0200

    Fix generics example for v2.

--- tests/roots/test-base/target/example_generics.py
+++ tests/roots/test-base/target/example_generics.py
@@ -1,7 +1,6 @@
 from typing import Generic, TypeVar, Optional, List
 
 from pydantic import BaseModel, field_validator
-from pydantic.generics import GenericModel
 
 DataT = TypeVar('DataT')
 
@@ -18,13 +17,13 @@ class DataModel(BaseModel):
     people: List[str]
 
 
-class Response(GenericModel, Generic[DataT]):
+class Response(BaseModel, Generic[DataT]):
     """HTTP Response representation."""
 
     data: Optional[DataT]
     error: Optional[Error]
 
-    @field_validator('error', always=True)
+    @field_validator('error')
     def check_consistency(cls, v, values):
         if v is not None and values['data'] is not None:
             raise ValueError('must not provide both data and error')
