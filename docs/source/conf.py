afe28cb65ad621e983a9491bfea105d292827bac
Franz Wöllert <franz.woellert@heidelberg.com>
2023-08-01 23:26:39 +0200

    Update changelog. Bump version - skip ci.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,11 +27,11 @@ sys.path.insert(0, str(path_autodoc_pydantic))
 # -- Project information -----------------------------------------------------
 
 project = 'autodoc_pydantic'
-copyright = '2022, Franz Wöllert'
+copyright = '2023, Franz Wöllert'
 author = 'Franz Wöllert'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 # -- General configuration ---------------------------------------------------
 
