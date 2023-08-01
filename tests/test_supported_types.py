43dd27db1ade55ec4cc5efcae194685b807b16af
Franz Wöllert <franz.woellert@heidelberg.com>
2023-08-01 21:29:15 +0200

    Add explicit tests for supported types.

---
+++ tests/test_supported_types.py
@@ -0,0 +1,18 @@
+import pytest
+
+from sphinxcontrib.autodoc_pydantic import PydanticModelDocumenter
+
+@pytest.mark.parametrize("model_name", argvalues=["CookingModel"])
+def test_autodoc_pydantic_model_no_exception(autodocument, model_name):
+    """Only ensure that the model name is correct in the generated rst. This
+    is sufficient to ensure that the model can be documented without raising
+    an exception.
+
+    """
+
+    kwargs = dict(object_path='target.supported_types.CookingModel',
+                  documenter=PydanticModelDocumenter.objtype)
+
+    # explicit global
+    actual = autodocument(**kwargs)
+    assert actual[1] == f'.. py:pydantic_model:: {model_name}'
