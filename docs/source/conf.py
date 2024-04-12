6102f990e6228cb4dda7a3e0f986dfcb0aa2e1ab
Franz Wöllert <franz.woellert@gmail.com>
2024-04-12 11:10:09 +0200

    Drop sphinx < 4 and pin versions for CI (#249)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2022, Franz Wöllert'
 author = 'Franz Wöllert'
 
 # The full version, including alpha/beta/rc tags
-release = '1.9.0'
+release = '1.9.1'
 
 # -- General configuration ---------------------------------------------------
 
